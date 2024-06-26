3b59b8d8bb7096ed18d7db36027ed27fc5173ae1
mfauzidr <88012389+mfauzidr@users.noreply.github.com>
2024-06-27 01:37:12 +0700

    cloud env logs

--- src/config/cloud.ts
+++ src/config/cloud.ts
@@ -4,15 +4,16 @@ import * as dotenv from "dotenv"
 let path = "./.env.production"
 dotenv.config({ path })
 
+console.log('Cloud Name:', process.env.CLOUD_NAME);
+console.log('Cloud Key:', process.env.CLOUD_KEY);
+console.log('Cloud Secret:', process.env.CLOUD_SECRET);
+
 const cloudConfig: ConfigOptions = {
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_KEY,
   api_secret: process.env.CLOUD_SECRET,
   secure: true,
 };
-console.log(process.env.CLOUD_KEY);
-console.log(process.env.CLOUD_NAME);
-console.log(process.env.CLOUD_SECRET);
 
 cloudinary.config(cloudConfig);
 
